a17486ea1ca0c2f6cb877e920685a36e8f595abc
Christian Lorenzo <clbmiami2004@gmail.com>
2020-10-06 22:33:52 -0700

    Update single_number.py
    Completed Single Number solution and it's passing all tests.

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,11 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-
-    pass
+    result = 0
+    
+    for n in arr:
+        result ^= n
+    return result
 
 
 if __name__ == '__main__':
